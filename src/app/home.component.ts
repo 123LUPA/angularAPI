2b757301271d1666dc9c8033f61272f1123d789b
Klucinka007 <Palkovicova.lucia@gmail.com>
2016-11-14 14:18:46 +0100

    API works!

---
+++ src/app/home.component.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by Palko on 11/10/2016.
+ */
+
+
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'home',
+  template:`this is home website`
+})
+
+export class HomeComponent{
+
+}
